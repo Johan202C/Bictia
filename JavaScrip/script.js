65b26a7cc9e388e2171a02f5b73e9e90168c67df
Johan202C <49340642+Johan202C@users.noreply.github.com>
2020-04-28 08:06:58 -0500

    Se modifican archivos sin importancia y se sube archivos de clase javascript

---
+++ JavaScrip/script.js
@@ -0,0 +1,53 @@
+/* Console.log para mostrar en consola */
+console.log("HOla mundo")
+/* Para mostrar mensajes en el navegador */
+alert("Hola")
+
+/* En javascrpt una variable puede gueardar string, booleano, int
+
+-lenguaje de debil tipado se redefinen .
+-lenguaje de fuerte tipado se.
+
+DECLARACIONES VARIABLES------------
+
+
+var:tiene un alcance global a nivel de ventana.
+const : variable que siempre sera constante.
+let:tiene mayor alcance: es mejor usarlo.
+ */
+
+ /* para mostrar el tamano de la pantalla */
+ const height=window.outerHeight
+ console.log(height)
+
+ /* Tipos de datos */
+
+ /* PRIMITIVOS */
+ //number : int, float, etc
+ //string : cualquier tipo de texto
+ //boolean :solo recibe dos valores TRUE/FALSE
+ //null: Valor ninguno
+ //undefined :una variable que se declara pero no recibe valor
+
+ const numero = 321;//number
+ const text = "Hola mundo soy una cosntante";//string
+ const verdadero = true; //boolean
+ const falso = false; //boolean
+
+ let estudiantes;//undefinded
+ const ejemploNull = null;//undefidend
+
+/*  Sirve para saber que tipo de dato es : typeof */
+ console.log(typeof numero)
+ console.log(typeof text)
+ console.log(typeof verdadero)
+ console.log(typeof falso)
+
+
+/* COMPLEJOS */
+//Array
+const estDeLaClase = ["Leonardo","Jesus","Tatiana","johan"];
+ 
+console.log(estDeLaClase[4]);
+
+
