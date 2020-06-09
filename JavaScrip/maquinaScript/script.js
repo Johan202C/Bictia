e71b9e8e936b351ce444128d1532f789522d7515
Johan202C <49340642+Johan202C@users.noreply.github.com>
2020-06-09 08:43:29 -0500

    Se actualiza maquina de escribir y se modifica proyecto artista

---
+++ JavaScrip/maquinaScript/script.js
@@ -0,0 +1,18 @@
+//const areaTex = document.querySelector(".areaTex");
+
+//console.log(areaTex);
+
+const num1 = document.querySelector(".num1").Value;
+console.log(num1);
+
+const fincio = () =>{
+    const 
+    const num1 = document.querySelector(".num1").Value;
+    console.log(num1);
+    const areaTex = document.querySelector(".areaTex");
+    const textP = document.querySelector(".textP");
+    newLetter.innerText=textP;
+   
+    
+}
+//addLetter.addEventListener('click',fincio);
